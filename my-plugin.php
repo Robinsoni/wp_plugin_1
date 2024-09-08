8b240245da9df04f3da33bbd796a2134df1eb936
Robin Soni <sonirobin369@gmail.com>
2024-09-08 23:55:39 +0530

    add activation and deactivation functionality

--- my-plugin.php
+++ my-plugin.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
 Plugin Name: my plugin dev
 Plugin URI:  
@@ -10,29 +10,53 @@ Requires PHP: 7.4
 Author URI: 
 Text Domain: my-plugin
 Domain Path: /languages/
- * */ 
-if(!defined(('ABSPATH'))){
+ * */
+if (!defined(('ABSPATH'))) {
     header("Location: /my_plugin");
     die("Can't access");
 }
 
-function my_plugin_activation(){
-   
-}
-function my_plugin_deactivation(){
+function my_plugin_activation()
+{
+    global $wpdb, $table_prefix;
+    $wp_emp  = $table_prefix . 'emp';
+    $q = "CREATE TABLE IF NOT EXISTS
+    `$wp_emp` (
+            `ID` INT NOT NULL AUTO_INCREMENT,
+            `name` VARCHAR(50) NOT NULL,
+            `email` VARCHAR(100) NOT NULL,
+            `status` BOOLEAN NOT NULL,
+            PRIMARY KEY (`ID`)
+        ) ENGINE = MyISAM; ";
+    $wpdb->query($q);
 
+    $data = array(
+        'name' => 'Ak',
+        'email' => 'ak@gmail.com',
+        'status' => 1,
+    );
+    $wpdb->insert($wp_emp,$data);
+}
+function my_plugin_deactivation()
+{
+    global $wpdb, $table_prefix;
+    $wp_emp = $table_prefix.'emp';
+    $q = "TRUNCATE `$wp_emp`";
+    $wpdb->query($q);
+    
 }
 register_activation_hook(
-	__FILE__,
-	'my_plugin_activation'
+    __FILE__,
+    'my_plugin_activation'
 );
 
 register_deactivation_hook(
-	__FILE__,
-	'my_plugin_deactivation'
+    __FILE__,
+    'my_plugin_deactivation'
 );
 
-function my_sc_fun(){
+function my_sc_fun()
+{
     return 'Function Call';
 }
-add_shortcode('my-sc','my_sc_fun');
+add_shortcode('my-sc', 'my_sc_fun');
--- uninstall.php
+++ uninstall.php
@@ -3,3 +3,8 @@ if(!defined('WP_UNINSTALL_PLUGIN')){
     header("Location: /test");
     die(" uninstallation");
 }
+
+global $wpdb, $table_prefix;
+$wp_emp = $table_prefix.'emp';
+$q = "DROP TABLE `$wp_emp`;";
+$wpdb->query();
