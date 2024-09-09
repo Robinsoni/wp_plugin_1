92deab292ec447875f8b75e1477e377707c8d2bc
Robin Soni <sonirobin369@gmail.com>
2024-09-09 18:14:27 +0530

    short code to show all the posts.

--- my-plugin.php
+++ my-plugin.php
@@ -82,14 +82,76 @@ function my_custom_scripts()
     // is_user_logged_in(); -  this helps you check whether the user is logged in or not
     wp_enqueue_style('my-custom-style', $path_style, "", $ver_style); // true means add it in footer after body tag
     wp_enqueue_script('my-custom-js', $path_js, $dep, $ver, true); // true means add it in footer after body tag
-    wp_add_inline_script('my-custom-js','var is_login = '.is_user_logged_in().';','before'); 
-    
+    wp_add_inline_script('my-custom-js', 'var is_login = ' . is_user_logged_in() . ';', 'before');
+
     /** Let's say we want to include to only one specific page then how would that work */
-    if(is_page('home')){
+    if (is_page('home')) {
         wp_enqueue_script('my-custom-js', $path_js, $dep, $ver, true); // true means add it in footer after body tag 
     }
-
 }
 add_action("wp_enqueue_scripts", 'my_custom_scripts');
 
 // ifyou want to enqueue scripts in admin panel then you can use admin enqueue scripts 
+
+function shortcode_select()
+{
+    global $wpdb, $table_prefix;
+    $wp_emp  = $table_prefix . 'emp';
+    $q = "SELECT * FROM `$wp_emp`;";
+    $results = $wpdb->get_results($q);
+    ob_start();
+?>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($results as $row) :
+            ?>
+                <tr>
+                    <td><?php echo $row->ID; ?></td>
+                    <td><?php echo $row->name; ?></td>
+                    <td><?php echo $row->email; ?></td>
+                    <td><?php echo $row->status; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+<?php
+    $html = ob_get_clean();
+    return $html;
+}
+add_shortcode('wp_sc_select', "shortcode_select");
+
+function my_posts()
+{
+    $args = array(
+        'post_type' => 'post'
+    ); 
+    $the_query = new WP_Query($args); 
+    // The Loop.
+    ob_start();
+   ?><h1> Let's see all the posts</h1><?php
+    if ($the_query->have_posts()) {
+        echo '<ul>';
+        while ($the_query->have_posts()) {
+            $the_query->the_post();
+            echo '<li>' . esc_html(get_the_title()) . '</li>';
+        }
+        echo '</ul>';
+    } else {
+        esc_html_e('Sorry, no posts matched your criteria.');
+    }
+    // Restore original Post Data.
+    wp_reset_postdata();
+    $html = ob_get_clean();
+    return $html;
+}
+add_shortcode('my-posts', "my_posts");
