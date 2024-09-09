c3726bff83d118cf2502b44fc95cbf7fe6cf5a06
Robin Soni <sonirobin369@gmail.com>
2024-09-09 13:30:14 +0530

    Include and enqueue js and css files.

--- my-plugin.php
+++ my-plugin.php
@@ -35,15 +35,14 @@ function my_plugin_activation()
         'email' => 'ak@gmail.com',
         'status' => 1,
     );
-    $wpdb->insert($wp_emp,$data);
+    $wpdb->insert($wp_emp, $data);
 }
 function my_plugin_deactivation()
 {
     global $wpdb, $table_prefix;
-    $wp_emp = $table_prefix.'emp';
+    $wp_emp = $table_prefix . 'emp';
     $q = "TRUNCATE `$wp_emp`";
     $wpdb->query($q);
-    
 }
 register_activation_hook(
     __FILE__,
@@ -60,7 +59,7 @@ function my_sc_fun($atts)
     $atts = array_change_key_case($atts, CASE_LOWER);
     $atts = shortcode_atts(array(
         'test' => "this is default value"
-    ),$atts);
+    ), $atts);
 
     /* 
     ob_start();
@@ -71,8 +70,26 @@ function my_sc_fun($atts)
 }
 /* add_shortcode('my-sc', 'my_sc_fun'); */
 
-$path = plugins_url('js/main.js',__FILE__);
+function my_custom_scripts()
+{
 
- $dep = array('jquery');
- $ver = filemtime(plugin_dir_path(__FILE__).'js/main.js'); // making it dynamic so that cache gets updated everytime the file is modified.
-  wp_enqueue_script('my-custom-js',$path, $dep,$ver,true); // true means add it in footer after body tag
+    $path_js = plugins_url('js/main.js', __FILE__);
+    $path_style = plugins_url('css/style.css', __FILE__);
+
+    $dep = array('jquery');
+    $ver = filemtime(plugin_dir_path(__FILE__) . 'js/main.js'); // making it dynamic so that cache gets updated everytime the file is modified.
+    $ver_style = filemtime(plugin_dir_path(__FILE__) . 'css/style.css'); // making it dynamic so that cache gets updated everytime the file is modified.
+    // is_user_logged_in(); -  this helps you check whether the user is logged in or not
+    wp_enqueue_style('my-custom-style', $path_style, "", $ver_style); // true means add it in footer after body tag
+    wp_enqueue_script('my-custom-js', $path_js, $dep, $ver, true); // true means add it in footer after body tag
+    wp_add_inline_script('my-custom-js','var is_login = '.is_user_logged_in().';','before'); 
+    
+    /** Let's say we want to include to only one specific page then how would that work */
+    if(is_page('home')){
+        wp_enqueue_script('my-custom-js', $path_js, $dep, $ver, true); // true means add it in footer after body tag 
+    }
+
+}
+add_action("wp_enqueue_scripts", 'my_custom_scripts');
+
+// ifyou want to enqueue scripts in admin panel then you can use admin enqueue scripts 
