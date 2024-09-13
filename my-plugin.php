97c0498a31b0fddef3ddf4117a36aa7f65e8b7c8
Robin Soni <sonirobin369@gmail.com>
2024-09-13 11:11:03 +0530

    added genere and writer taxonomies

--- my-plugin.php
+++ my-plugin.php
@@ -321,3 +321,60 @@ function create_custom_post_type() {
 
     register_post_type('book', $args);
 }
+
+/** Write book taxonomy  */
+add_action('init', 'create_book_taxonomies', 0);
+
+// Create two taxonomies, Genres (hierarchical like categories) and Writers (non-hierarchical like tags)
+function create_book_taxonomies() {
+    // Add Genre taxonomy (like categories)
+    $labels = array(
+        'name'              => _x('Genres', 'taxonomy general name'),
+        'singular_name'     => _x('Genre', 'taxonomy singular name'),
+        'search_items'      => __('Search Genres'),
+        'all_items'         => __('All Genres'),
+        'parent_item'       => __('Parent Genre'),
+        'parent_item_colon' => __('Parent Genre:'),
+        'edit_item'         => __('Edit Genre'),
+        'update_item'       => __('Update Genre'),
+        'add_new_item'      => __('Add New Genre'),
+        'new_item_name'     => __('New Genre Name'),
+        'menu_name'         => __('Genre'),
+    );
+
+    $args = array(
+        'hierarchical'      => true, // True makes it like categories
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array('slug' => 'genre'),
+    );
+
+    register_taxonomy('genre', array('book'), $args);
+
+    $labels = array(
+        'name'                       => _x('Writers', 'taxonomy general name'),
+        'singular_name'              => _x('Writer', 'taxonomy singular name'),
+        'search_items'               => __('Search Writers'),
+        'all_items'                  => __('All Writers'),
+        'parent_item'                => null,
+        'parent_item_colon'          => null,
+        'edit_item'                  => __('Edit Writer'),
+        'update_item'                => __('Update Writer'),
+        'add_new_item'               => __('Add New Writer'),
+        'new_item_name'              => __('New Writer Name'),
+        'menu_name'                  => __('Writers'),
+    );
+    
+    $args = array(
+        'hierarchical'          => true, // Set to true to make it like categories with checkboxes
+        'labels'                => $labels,
+        'show_ui'               => true,
+        'show_admin_column'     => true,
+        'query_var'             => true,
+        'rewrite'               => array('slug' => 'writer'),
+    );
+    
+    register_taxonomy('writer', 'book', $args);
+}
